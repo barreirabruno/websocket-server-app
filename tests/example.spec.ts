990fd33f020e687298d1f82694d4fb0df1934d00
brunobarreira <brunobarreirasilva@gmail.com>
2022-06-29 10:10:24 -0300

    test: validate watch script with a second unit test

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,4 +2,8 @@ describe("This is a example test, delete it to proceed with development process"
   it("should execute a simple sum", () => {
     expect(2+2).toBe(4)
   })
+
+  it("should execute a simple subtraction", () => {
+    expect(5-2).toBe(3)
+  })
 })
