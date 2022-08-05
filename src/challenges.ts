7dcdfcf7b9dc71ec74007e8eb0aad1298ce8fcd9
brunobarreira <brunobarreirasilva@gmail.com>
2022-08-05 17:58:10 -0300

    feat: add static ids to challenges

--- src/challenges.ts
+++ src/challenges.ts
@@ -1,5 +1,6 @@
 export const challenges = [
   {
+    id: 'c8c8a153-e18c-4020-8135-1f4b46ee6302',
     question: 'O cantor Alípio Martins não faria uma canção, nem tocaria um violão para um peixe qualquer. Para qual peixe Alípio está cantando?',
     options: [
       'Peixe boi',
@@ -9,6 +10,7 @@ export const challenges = [
     correct: 'Piranha'
   },
   {
+    id: 'b055e8ab-c984-41d4-b339-3dc8694c9c8c',
     question: 'Qual a melhor maneira de fazer amizades virtuais segundo o apresentador Celso Portiolli?',
     options: [
       'Bate-papo virtual',
@@ -18,6 +20,7 @@ export const challenges = [
     correct: 'Bate-papo virtual'
   },
   {
+    id: '589cb070-5a2e-455c-8829-034077bfbc7c',
     question: 'Segundo o cantor Crazy frog em sua canção, o que o Crazy Frog quer?',
     options: [
       'ABLINBLONGBLONGBLONGBLONGBLONGBLONG',
@@ -27,6 +30,7 @@ export const challenges = [
     correct: 'BIN BIN'
   },
   {
+    id: 'fa0ce726-2bd2-498c-9e1e-57cd2dcda1a3',
     question: 'Qual das frases abaixo é uma vírgula sonora do programa Vai dar namoro?',
     options: [
       'Muito bem, Silvio',
@@ -36,6 +40,7 @@ export const challenges = [
     correct: 'Ele gosta'
   },
   {
+    id: '58d04619-2138-4850-bcd1-ff0a5a70ba62',
     question: 'Qual a frase utilizada pelo ator Theo Beker no reality show A Fazendo ao apresentar os músculos de seu corpo para outro participante?',
     options: [
       'Deu trabalho conseguir esses músculos',
@@ -45,6 +50,7 @@ export const challenges = [
     correct: 'Esse é irmão desse, que é irmão desse, que é irmão desse'
   },
   {
+    id: 'ee304eae-737e-4167-8d72-7490510eb0bd',
     question: 'Qual a maneira correta de reduzir o número de carros andando na rua?',
     options: [
       'Andar com o carro na calçada',
@@ -54,6 +60,7 @@ export const challenges = [
     correct: 'Andar com o carro na calçada'
   },
   {
+    id: '2abedcbf-2de4-4c0f-aa8f-69c4243591a2',
     question: 'Qual foi a fala do cantor Ovelha antes de dizer que - sem você não viverei?',
     options: [
       'AI AI AI AI',
