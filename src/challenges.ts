445fb0ab98e99070705b5551a07875a0ab21a52c
brunobarreira <brunobarreirasilva@gmail.com>
2022-08-05 11:50:47 -0300

    feat: add challenges content

---
+++ src/challenges.ts
@@ -0,0 +1,65 @@
+export const challenges = [
+  {
+    question: 'O cantor Alípio Martins não faria uma canção, nem tocaria um violão para um peixe qualquer. Para qual peixe Alípio está cantando?',
+    options: [
+      'Peixe boi',
+      'Baleia',
+      'Piranha'
+    ],
+    correct: 'Piranha'
+  },
+  {
+    question: 'Qual a melhor maneira de fazer amizades virtuais segundo o apresentador Celso Portiolli?',
+    options: [
+      'Bate-papo virtual',
+      'Envio de cartas',
+      'Falar com estranhos na rodoviária'
+    ],
+    correct: 'Bate-papo virtual'
+  },
+  {
+    question: 'Segundo o cantor Crazy frog em sua canção, o que o Crazy Frog quer?',
+    options: [
+      'ABLINBLONGBLONGBLONGBLONGBLONGBLONG',
+      'BRINGLINGLONGLONGLONGLONGLONGLONGLON',
+      'BIN BIN'
+    ],
+    correct: 'BIN BIN'
+  },
+  {
+    question: 'Qual das frases abaixo é uma vírgula sonora do programa Vai dar namoro?',
+    options: [
+      'Muito bem, Silvio',
+      'Ele gosta',
+      'Beijo do gordo'
+    ],
+    correct: 'Ele gosta'
+  },
+  {
+    question: 'Qual a frase utilizada pelo ator Theo Beker no reality show A Fazendo ao apresentar os músculos de seu corpo para outro participante?',
+    options: [
+      'Deu trabalho conseguir esses músculos',
+      'Você é frango, olha meus músculos',
+      'Esse é irmão desse, que é irmão desse, que é irmão desse'
+    ],
+    correct: 'Esse é irmão desse, que é irmão desse, que é irmão desse'
+  },
+  {
+    question: 'Qual a maneira correta de reduzir o número de carros andando na rua?',
+    options: [
+      'Andar com o carro na calçada',
+      'Usar Uber',
+      'Não sair de casa'
+    ],
+    correct: 'Andar com o carro na calçada'
+  },
+  {
+    question: 'Qual foi a fala do cantor Ovelha antes de dizer que - sem você não viverei?',
+    options: [
+      'AI AI AI AI',
+      'UÔ UÔ UÔ IÊ IÊ',
+      'UUUHHHHHHH'
+    ],
+    correct: 'UÔ UÔ UÔ IÊ IÊ'
+  }
+]
